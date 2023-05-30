658891d1e65d3542375779a555a6b51aa83b9e58
Lewis Trowbridge <lewbob9927@gmail.com>
2023-05-30 10:29:23 +0100

    Seperated words into dict and added basic output

--- scripts/json_translator.py
+++ scripts/json_translator.py
@@ -1,16 +1,22 @@
 import json
 from urllib.request import urlopen
 
-
 f = urlopen('https://raw.githubusercontent.com/thiagobodruk/bible/master/json/en_kjv.json')
-
 data = json.load(f)
+words = {}
 
 for i in data:
-    print(i["name"])
-    print(len(i["chapters"]))
     for chapter_num, chapter in enumerate(i["chapters"]):
         for verse_num, verse in enumerate(chapter):
-            print(f"{i['name']} {chapter_num + 1}:{verse_num + 1} - {verse}")
-            
+            for word in verse.split(" "):
+                word = word.title()
+                if word not in words.keys():
+                    words[word] = []
+                words[word].append({
+                    "book": i["name"],
+                    "chapter": chapter_num,
+                    "verse": verse_num
+                })
 
+print("Produced JSON:\n", words)
+print("Number of words:", len(words.keys()))
