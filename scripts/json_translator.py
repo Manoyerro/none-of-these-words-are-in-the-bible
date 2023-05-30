0df87ac4e5b2438af9f29f9536d43b7dcf9618ef
Lewis Trowbridge <lewbob9927@gmail.com>
2023-05-30 10:47:28 +0100

    Sanitised string with regex and wrote out to file

--- scripts/json_translator.py
+++ scripts/json_translator.py
@@ -1,6 +1,8 @@
 import json
+import re
 from urllib.request import urlopen
 
+sanitise_regex = r"[^\w*]"
 f = urlopen('https://raw.githubusercontent.com/thiagobodruk/bible/master/json/en_kjv.json')
 data = json.load(f)
 words = {}
@@ -9,14 +11,18 @@ for i in data:
     for chapter_num, chapter in enumerate(i["chapters"]):
         for verse_num, verse in enumerate(chapter):
             for word in verse.split(" "):
-                word = word.title()
+                word = re.sub(sanitise_regex, "", word.title())
                 if word not in words.keys():
                     words[word] = []
                 words[word].append({
                     "book": i["name"],
-                    "chapter": chapter_num,
-                    "verse": verse_num
+                    "chapter": chapter_num + 1,
+                    "verse": verse_num + 1
                 })
 
-print("Produced JSON:\n", words)
+json_string = json.dumps(words)
+print("Produced JSON:\n", json_string)
 print("Number of words:", len(words.keys()))
+with open("words.json", "w") as out:
+    out.write(json_string)
+print("Written to words.json.")
