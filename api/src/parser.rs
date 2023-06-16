452c815185d6eb109d2e7935992f148b81fb8e77
Lewis Trowbridge <lewbob9927@gmail.com>
2023-06-16 11:34:30 +0100

    Added test for parser and proper error message

--- api/src/parser.rs
+++ api/src/parser.rs
@@ -8,7 +8,18 @@ pub struct WordInfo {
     verse: u8,
 }
 
-pub async fn get_file_as_map() -> HashMap<String, Vec<WordInfo>> {
-    let words_string = std::fs::read_to_string("assets/words.json").unwrap();
+pub fn get_file_as_map() -> HashMap<String, Vec<WordInfo>> {
+    let words_string = std::fs::read_to_string("assets/words.json").expect("Words file not found");
     serde_json::from_str(&words_string).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::get_file_as_map;
+
+    #[test]
+    fn get_file_as_map_returns_file() {
+        let map = get_file_as_map();
+        assert_eq!(map.len(), 13493)
+    }
 }
