2675fd86f7f2149f452e5e2b9d5763d3187424bf
Lewis Trowbridge <lewbob9927@gmail.com>
2023-06-26 16:42:05 +0100

    WIP Used map function to lookup words

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -4,22 +4,29 @@ use crate::models::words::Words;
 use crate::parser::get_file_as_map;
 use actix_web::web;
 use actix_web::{web::Json, HttpResponse};
+use std::collections::HashMap;
 
 pub async fn get_frequency(words: Json<Words>) -> HttpResponse {
     // TODO: See if we can move this to a static location
     let bible_words = get_file_as_map();
-    let mut word_freq: Vec<ReturnInfo> = Vec::new();
     let split_words = words.words.split(" ");
-    for word in split_words {
-        let found_word_info = bible_words.get(word);
-        if found_word_info.is_some() {
-            // Create return info from found word
-            word_freq.push(ReturnInfo {
-                matches: Vec::from_iter(found_word_info.unwrap().iter()),
-            });
-        }
-    }
+    let word_freq: Vec<Option<ReturnInfo>> =
+        split_words.map(|word| lookup(&bible_words, word)).collect();
+
     HttpResponse::Ok().json(word_freq)
+}
+
+fn lookup<'a>(
+    bible_words: &'a HashMap<String, Vec<WordInfo>>,
+    word: &str,
+) -> Option<ReturnInfo<'a>> {
+    let found_word_info = bible_words.get(word);
+    match found_word_info {
+        Some(info) => Some(ReturnInfo {
+            matches: Vec::from_iter(info.iter()),
+        }),
+        None => None,
+    }
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
@@ -42,7 +49,7 @@ mod tests {
         let test_words = Words {
             words: "I miss Fauna".to_string(),
         };
-        let json_words = web::Json(test_words);
+        let json_words = Json(test_words);
         let resp = get_frequency(json_words).await;
         assert_eq!(resp.status(), http::StatusCode::OK);
     }
@@ -69,7 +76,7 @@ mod tests {
                 verse: 1,
             },
         ];
-        let json_words = web::Json(test_words);
+        let json_words = Json(test_words);
         let resp = get_frequency(json_words).await;
         let byte_body = to_bytes(resp.into_body()).await.unwrap();
         let string_body = from_utf8(&byte_body).unwrap();
