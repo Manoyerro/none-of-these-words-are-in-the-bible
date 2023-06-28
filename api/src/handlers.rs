03a4fd3193321e8b8afbd9d38005d70b836f5f2f
Lewis Trowbridge <lewbob9927@gmail.com>
2023-06-28 17:11:53 +0100

    Added and altered tests for new functionality

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -46,7 +46,7 @@ mod tests {
     use std::str::from_utf8;
 
     #[actix_web::test]
-    async fn test_response_ok() {
+    async fn test_formatted_response_code_200_ok() {
         let test_words = Words {
             words: "I miss Fauna".to_string(),
         };
@@ -56,27 +56,32 @@ mod tests {
     }
 
     #[actix_web::test]
-    async fn test_body_ok() {
+    async fn test_missing_word_returns_empty_array() {
         let test_words = Words {
-            words: "I miss Fauna".to_string(),
+            words: "Fauna".to_string(),
         };
-        let expected_output: Vec<WordInfo> = vec![
-            WordInfo {
-                book: "I".to_string(),
-                chapter: 1,
-                verse: 1,
-            },
-            WordInfo {
-                book: "miss".to_string(),
-                chapter: 1,
-                verse: 1,
-            },
-            WordInfo {
-                book: "Fauna".to_string(),
-                chapter: 1,
-                verse: 1,
-            },
-        ];
+        let expected_output: Vec<ReturnInfo> = Vec::new();
+        let json_words = Json(test_words);
+        let resp = get_frequency(json_words).await;
+        let byte_body = to_bytes(resp.into_body()).await.unwrap();
+        let string_body = from_utf8(&byte_body).unwrap();
+        assert_eq!(string_body, json!(expected_output).to_string())
+    }
+
+    #[actix_web::test]
+    async fn test_present_word_returns_array_of_return_info() {
+        let test_words = Words {
+            // For some reason, this is one of the only words that only appears once in the bible/
+            words: "Girl".to_string(),
+        };
+        let expected_info = WordInfo {
+            book: "Joel".to_string(),
+            chapter: 3,
+            verse: 3,
+        };
+        let expected_output: Vec<ReturnInfo> = vec![ReturnInfo {
+            matches: vec![&expected_info],
+        }];
         let json_words = Json(test_words);
         let resp = get_frequency(json_words).await;
         let byte_body = to_bytes(resp.into_body()).await.unwrap();
