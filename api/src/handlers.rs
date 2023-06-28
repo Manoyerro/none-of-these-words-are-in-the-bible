70bbbe429784f5f564bfbcd211db84a0b40aa4cc
Lewis Trowbridge <lewbob9927@gmail.com>
2023-06-28 18:46:06 +0100

    Added extra tests for multiple words

--- api/src/handlers.rs
+++ api/src/handlers.rs
@@ -10,7 +10,7 @@ use std::collections::HashMap;
 static BIBLE_WORDS: Lazy<HashMap<String, Vec<WordInfo>>> = Lazy::new(|| get_file_as_map());
 
 pub async fn get_frequency(words: Json<Words>) -> HttpResponse {
-    let split_words = words.words.split(" ");
+    let split_words = words.words.split_whitespace();
     let word_freq: Vec<ReturnInfo> = split_words
         .map(|word| lookup(word))
         .filter(|item| item.is_some())
@@ -71,6 +71,51 @@ mod tests {
         let test_words = Words {
             // For some reason, this is one of the only words that only appears once in the bible/
             words: "Girl".to_string(),
+        };
+        let expected_info = WordInfo {
+            book: "Joel".to_string(),
+            chapter: 3,
+            verse: 3,
+        };
+        let expected_output: Vec<ReturnInfo> = vec![ReturnInfo {
+            matches: vec![&expected_info],
+        }];
+        let json_words = Json(test_words);
+        let resp = get_frequency(json_words).await;
+        let byte_body = to_bytes(resp.into_body()).await.unwrap();
+        let string_body = from_utf8(&byte_body).unwrap();
+        assert_eq!(string_body, json!(expected_output).to_string())
+    }
+
+    #[actix_web::test]
+    async fn test_repeated_present_word_returns_array_of_return_info() {
+        let test_words = Words {
+            words: "Girl Girl".to_string(),
+        };
+        let expected_info = WordInfo {
+            book: "Joel".to_string(),
+            chapter: 3,
+            verse: 3,
+        };
+        let expected_output: Vec<ReturnInfo> = vec![
+            ReturnInfo {
+                matches: vec![&expected_info],
+            },
+            ReturnInfo {
+                matches: vec![&expected_info],
+            },
+        ];
+        let json_words = Json(test_words);
+        let resp = get_frequency(json_words).await;
+        let byte_body = to_bytes(resp.into_body()).await.unwrap();
+        let string_body = from_utf8(&byte_body).unwrap();
+        assert_eq!(string_body, json!(expected_output).to_string())
+    }
+
+    #[actix_web::test]
+    async fn test_present_and_missing_word_returns_array_of_return_info() {
+        let test_words = Words {
+            words: "Girl Fauna".to_string(),
         };
         let expected_info = WordInfo {
             book: "Joel".to_string(),
