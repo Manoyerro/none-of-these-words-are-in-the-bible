9663b26c86d7148b8d5209a322d04faf87965d13
Lewis Trowbridge <lewbob9927@gmail.com>
2023-07-31 13:50:28 +0100

    Set address to 0.0.0.0 in production

--- api/src/main.rs
+++ api/src/main.rs
@@ -4,10 +4,19 @@ mod parser;
 
 use actix_web::{middleware, App, HttpServer};
 use std::env;
+use std::net::{Ipv4Addr, SocketAddrV4};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Get port from $PORT, defaulting to 8000
     let port = env::var("PORT").unwrap_or_default().parse().unwrap_or(8000);
+    // Get address, hosting on 0.0.0.0 when PORT is set (assuming production), and defaulting to 127.0.0.1
+    let address = if env::var("PORT").is_ok() {
+        SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), port)
+    } else {
+        SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port)
+    };
+
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
 
@@ -16,7 +25,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .configure(handlers::config)
     })
-    .bind(("127.0.0.1", port))?
+    .bind(address)?
     .run()
     .await
 }
